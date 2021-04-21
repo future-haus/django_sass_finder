5572b37cede204152a7c6e57c1fb43bf0da43377
Kevin Morey <kevin@futurehaus.com>
2021-04-21 09:25:17 -0500

    Update README and fix files that don't match

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # django_sass_finder
 a Django finder that compiles Sass files
 
-## installation
+## Installation
 ### WARNING: MAKE SURE YOU HAVE NO SASS PACKAGES INSTALLED (other than libsass)!
+
 run `pip install django_sass_finder`, then put django_sass_finder in your `INSTALLED_APPS`, and
 finally list your static file finders as so:
+
 ```py
 STATICFILES_FINDERS = [
     # add the default Django finders as this setting will override the default
@@ -15,8 +17,30 @@ STATICFILES_FINDERS = [
 ]
 ```
 
-## usage
+## Configuration
+The following variables need to bet set in your settings file:
+
+```py
+SCSS_ROOT = 'web/src/scss'
+SCSS_COMPILE = ['styles.scss']
+```
+
+If you are including some node_modules or other files in your scss files, you can specify where to look for these:
+
+```py
+SCSS_INCLUDE_PATHS = ['node_modules']
+```
+
+### Optional settings 
+
+django_sass_finder defaults to using a temporary directory, but if you want to keep the css files around after compile and collect you can specify your own:
+
+```py
+CSS_COMPILE_DIR = 'tmp'
+```
+
+## Usage
 run `python manage.py collectstatic` to compile your Sass files and put them in the STATIC_ROOT.
 
-## license
-this package is licensed under the MIT license.
+## License
+This package is licensed under the MIT license.
--- django_sass_finder/finders.py
+++ django_sass_finder/finders.py
@@ -3,6 +3,9 @@ from django.contrib.staticfiles.finders import BaseFinder
 from django.core.files.storage import FileSystemStorage
 from django.core.checks import Error
 import sass
+import pathlib
+import tempfile
+import os
 
 class ScssFinder(BaseFinder):
     """
@@ -10,12 +13,13 @@ class ScssFinder(BaseFinder):
     """
 
     def __init__(self, *_args, **_kwargs):
-        try:
-            self.scss_compile = settings.SCSS_COMPILE
-        except AttributeError:
-            self.scss_compile = []
-        self.root = settings.SCSS_ROOT
-        self.storage = FileSystemStorage(settings.CSS_COMPILE_DIR)
+        self.scss_compile = getattr(settings, 'SCSS_COMPILE', [])
+        self.root = pathlib.Path(settings.SCSS_ROOT)
+        self.css_compile_dir = pathlib.Path(getattr(settings, 'CSS_COMPILE_DIR', tempfile.gettempdir()))
+        self.storage = FileSystemStorage(self.css_compile_dir)
+        self.include_paths = getattr(settings, 'SCSS_INCLUDE_PATHS', None)
+
+        self._find_matching_files()
 
     def check(self, **kwargs):
         """
@@ -38,21 +42,40 @@ class ScssFinder(BaseFinder):
         """
         Look for files in SCSS_ROOT, and make their paths absolute.
         """
-        if all:
-            return str([settings.CSS_COMPILE_DIR / path])
-        return str(settings.CSS_COMPILE_DIR / path)
+        for (css_file, scss_file) in self.matching_files:
+            if path == css_file:
+                filename = str(self.css_compile_dir / path)
+                self._compile_scss(css_file, scss_file)
+                if os.path.exists(filename):
+                    return [filename] if all else filename
 
     def list(self, _ignore_patterns):
         """
         Compile then list the .css files.
         """
+        for (css_file, scss_file) in self.matching_files:
+            yield self._compile_scss(css_file, scss_file)
+
+    def _find_matching_files(self):
+        self.matching_files = []
         for scss_item in self.scss_compile:
             abspath = self.root / scss_item
             if abspath.is_file():
-                abspath = self.root / scss_item
+                css_file = abspath.stem + '.css'
                 abspath_str = str(abspath)
-                outpath = settings.CSS_COMPILE_DIR / (abspath.stem + '.css')
-                with open(outpath, 'w+') as outfile:
-                    outfile.write(sass.compile(filename=abspath_str, output_style='compressed'))
-                print(settings.BASE_DIR)
-                yield abspath.stem + '.css', self.storage
+
+                self.matching_files.append(
+                    (css_file, abspath_str)
+                )
+                
+    def _compile_scss(self, css_file, scss_file):
+        outpath = self.css_compile_dir / css_file
+        args = {
+            'filename': scss_file,
+            'output_style': 'compressed',
+            'include_paths': self.include_paths, 
+            'source_map_contents': True
+        }
+        with open(outpath, 'w+') as outfile:
+            outfile.write(sass.compile(**args))
+        return css_file, self.storage
